237fd59b4b33aa64c444c6bab5fc008d8aa07231
Mervin Hemaraju <mervinhemaraju16@gmail.com>
2020-12-14 03:37:19 +0400

    - Heroku Testing

--- run.py
+++ run.py
@@ -2,4 +2,6 @@ from app import socketio, app
 
 # Start the app
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0")
+    from os import environ
+
+    socketio.run(app, host="0.0.0.0", port=environ.get("PORT", 5000))
